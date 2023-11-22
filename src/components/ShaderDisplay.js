52b25e21e232aa90f9a5649b35a8ddc696837df4
Malak Janus <992688+malakada@users.noreply.github.com>
2023-11-22 13:17:01 -0700

    PoC: Pull in shadertoy code and display from a single id

---
+++ src/components/ShaderDisplay.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import ShadertoyReact from 'shadertoy-react';
+
+export default function ShaderDisplay({ shaderToyId }) {
+  const [shaderToyCode, setShaderToyCode] = useState('');
+  const [shaderToyName, setShaderToyName] = useState('');
+  const [shaderToyAuthor, setShaderToyAuthor] = useState('');
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://www.shadertoy.com/api/v1/shaders/${shaderToyId}?key=BtHjRr`)
+      .then((response) => response.json())
+      .then((data) => {
+        setShaderToyCode(data.Shader.renderpass[0].code);
+        setShaderToyName(data.Shader.name);
+        setShaderToyAuthor(data.Shader.username);
+      })
+      .catch((error) => {
+        setError(error);
+      }
+    );
+  }, [shaderToyId]);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!shaderToyCode) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="ShaderDisplay">
+      <ShadertoyReact fs={shaderToyCode} />
+    </div>
+  );
+}
