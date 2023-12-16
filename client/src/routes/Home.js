e8a4de88ec86d7274defa207e17ad4a62f47d52a
Malak Janus <992688+malakada@users.noreply.github.com>
2023-12-15 18:38:06 -0700

    Add a hero area to the Home view

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,9 +1,27 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 import MainNav from '../components/MainNav';
 
 function Home() {
   return (
     <div>
       <MainNav />
+      <Container fluid className="hero-area">
+        <Row>
+          <Col className="d-flex">
+            <div className="hero-box mx-auto">
+              <h1>Room Code</h1>
+              <Form.Label htmlFor="roomCode">Enter a room code:</Form.Label>
+              <Form.Control id="roomCode" type="text" placeholder="Room code" />
+              <Button className="mt-4" variant="primary">Play</Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
